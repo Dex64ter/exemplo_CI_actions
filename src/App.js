65aed63e768581d8ede390db72376277a68cbecc
Davi J <davijosantos.dj@gmail.com>
2023-02-11 19:20:49 -0300

    change label

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teste na dev
+          Mudança de algo
         </p>
         <a
           className="App-link"
